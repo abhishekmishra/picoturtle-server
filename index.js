7ca08b8d5150d035250f8b6a3fe8eeb8f6cc1f05
Abhishek Mishra <abhishekmishra3@yahoo.com>
2019-02-27 08:40:42 +0530

    removed unused routes, add host config

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var turtleRouter = require('./routes/turtle');
 
 var app = express();
@@ -13,10 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'node_modules/picoturtle-client-js')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/turtle', turtleRouter);
 
 module.exports = app;
